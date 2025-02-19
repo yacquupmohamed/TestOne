cebbdb6d574027adaea5641c0baa8a26cdd425bd
yacquup <yacquup100@gmail.com>
2025-02-19 06:24:53 +0300

    wax ka badalka dhamn files kaga

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 document.getElementById('speakButton').addEventListener('click', () => {
-    // Hel qoraalka
+    
     const text = document.getElementById('inputText').value;
     
     if (text !== '') {
-        // U isticmaal Web Speech API SpeechSynthesis
+        
         const speech = new SpeechSynthesisUtterance(text);
         
-        // U dooro luqada iyo codka
-        speech.lang = 'so-SO'; // Tani waxay tahay luqadda Ingiriisiga, waxaad ka beddeli kartaa si ay u noqoto luqad kale
-        speech.rate = 1; // Xawaaraha hadalka (0.1 ilaa 10)
-        speech.pitch = 1; // Codka sare iyo hoose (0 - 2)
         
-        // Orod codka
+        speech.lang = 'so-SO'; 
+        speech.rate = 1; 
+        speech.pitch = 1; 
+        
+
         window.speechSynthesis.speak(speech);
     } else {
         alert('Fadlan wax qor si aad u dhageesato !');
